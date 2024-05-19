643223535532303486c72c514927c02132d3821b
Dmitry Egorov <info@consultapp.ru>
2024-05-20 00:32:28 +0400

    socket

--- app/socket.js
+++ app/socket.js
@@ -1,8 +1,4 @@
 "use client";
 import { io } from "socket.io-client";
 
-export const socket = io(
-  process.env.NODE_ENV === "production"
-    ? "ws://" + process.env.PRODUCTION_DOMAIN + "/socket"
-    : ""
-);
+export const socket = io("");
